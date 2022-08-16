ba28ec44d3586e916be07206f7ace547510296de
zhuang002 <zhuang001@gmail.com>
2022-08-16 11:39:28 -0400

    add loop,boolean,array examples

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,122 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		integerSamples();
 		
-		doubleSamples();
+		//integerSamples();
 		
-		/*arraySamples();
+		//doubleSamples();
+		
+		//arraySamples();
 		booleanSamples();
-		loopSamples();
-		*/
+		//loopSamples();
+		
+		/*int f = factorial(10); // 10!
+		System.out.println(f);
+		
+		f = factorial(15); // 15!
+		System.out.println(f);*/
+	}
+	
+	
+
+	private static void booleanSamples() {
+		// TODO Auto-generated method stub
+		boolean a,b,c;
+		
+		a= true;
+		b = false;
+		
+		c = !a;
+		System.out.println(c);
+		
+		c = a && b;
+		System.out.println(c);
+		
+		c = a || b;
+		System.out.println(c);
+		
+		a = 10 > 9;
+		b = 2 > 3;
+		
+		c = 4!=4;
+		
+		System.out.println(a+" "+b+" "+c);
+		
+		if (c) {
+			System.out.println("c is true");
+		} else {
+			System.out.println("c is false");
+		}
+		
+		if (a && b) {
+			System.out.println("a and b is true");
+		} else {
+			System.out.println("a and b is false");
+		}
+				
+		int i1,i2;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Input i1 and i2: ");
+		i1 = sc.nextInt();
+		i2 = sc.nextInt();
+		
+		a = i1>i2;
+		if (a) {
+			System.out.println("i1 is larger than i2");
+		} else if (i1<i2) {
+			System.out.println("i1 is smaller than i2");
+		} else {
+			System.out.println("i1 equals to i2");
+		}
+		
+	}
+
+
+
+	private static void arraySamples() {
+		// TODO Auto-generated method stub
+		int[] ia = new int[10];
+		int a=1;
+		int b=1;
+		for (int i=0;i<10;i++) {
+			a = a+i;
+			b = b+i;
+			ia[i] = a+b;
+		}
+		
+		for (int i=0;i<10;i++) {
+			System.out.print(ia[i]+" ");
+		}
+		
+		System.out.print(ia[12]);
+	}
+
+
+
+	private static int factorial(int n) {
+		// TODO Auto-generated method stub
+		int prod = 1;
+		for (int i=1;i<=n;i++) {
+			prod = prod * i;
+		}
+		return prod;
+	}
+
+	private static void loopSamples() {
+		// TODO 1+2+3...+100 --> 5050
+		int sum = 0;
+		for (int i=1;i<=10;i=i+1) { // for (init; loop-condition; updating) {}
+			sum = sum + i;
+		}
+		System.out.println(sum);
+		
 	}
 
 	private static void doubleSamples() {
 		// TODO Auto-generated method stub
 		System.out.println("---Double Sample ---");
 		double a,b,c;
+		
 		
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Please input a and b: ");
